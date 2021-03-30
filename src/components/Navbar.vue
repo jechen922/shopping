91d0e62584dc23fb24f8109a413de2ada0ed2df6
Robert <example@gmail.com>
2021-03-30 15:03:02 +0800

    加入登出功能

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,9 +8,30 @@
       <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
       <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
+          <a class="nav-link" href="#" @click.prevent="signout">Sign out</a>
         </li>
       </ul>
     </nav>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Navbar',
+  methods: {
+    signout () {
+      const vm = this
+      const api = `${process.env.API_PATH}/logout`
+      this.$http.post(api).then(response => {
+        if (response.data.success) {
+          const expired = Date.now()
+          document.cookie = `sessionToken=; expires=${new Date(expired)};`
+          vm.$router.push('/signin')
+        } else {
+          // todo: 加入錯誤訊息
+        }
+      })
+    }
+  }
+}
+</script>
