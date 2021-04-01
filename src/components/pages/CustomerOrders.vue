cf3a732d915216c1b01758299b6c9818a9b1a0f9
Robert <example@gmail.com>
2021-04-01 16:49:56 +0800

    加入收件資訊畫面

--- src/components/pages/CustomerOrders.vue
+++ src/components/pages/CustomerOrders.vue
@@ -80,51 +80,109 @@
     <pagination :page="pagination" v-on:emitGetProduct="getProducts"></pagination>
 
     <!-- 購物車 -->
-    <table class="table">
-      <thead>
-        <th></th>
-        <th>品名</th>
-        <th>數量</th>
-        <th>單價</th>
-      </thead>
-      <tbody>
-        <tr v-for="item in cart.carts" :key="item.id">
-          <td class="align-middle">
-            <button type="button" class="btn btn-outline-danger btn-sm"
-              @click="removeCartItem(item.id)">
-              <i class="far fa-trash-alt"></i>
-            </button>
-          </td>
-          <td class="align-middle">
-            {{ item.product.title }}
-            <div class="text-success" v-if="item.coupon">
-              已套用優惠券
+    <div v-if="cart.carts.length > 0">
+    <!-- <div> -->
+      <table class="table">
+        <thead>
+          <th></th>
+          <th>品名</th>
+          <th>數量</th>
+          <th>單價</th>
+        </thead>
+        <tbody>
+          <tr v-for="item in cart.carts" :key="item.id">
+            <td class="align-middle">
+              <button type="button" class="btn btn-outline-danger btn-sm"
+                @click="removeCartItem(item.id)">
+                <i class="far fa-trash-alt"></i>
+              </button>
+            </td>
+            <td class="align-middle">
+              {{ item.product.title }}
+              <div class="text-success" v-if="item.coupon">
+                已套用優惠券
+              </div>
+            </td>
+            <td class="align-middle">{{ item.qty }} / {{ item.product.unit }}</td>
+            <td class="align-middle text-right">{{ item.final_total }}</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="3" class="text-right">總計</td>
+            <td class="text-right">{{ cart.total }}</td>
+          </tr>
+          <tr v-if="cart.total != cart.final_total">
+            <td colspan="3" class="text-right text-success">折扣價</td>
+            <td class="text-right text-success">{{ cart.final_total }}</td>
+          </tr>
+        </tfoot>
+      </table>
+      <div class="input-group mb-3 input-group-sm">
+        <input type="text" class="form-control" placeholder="請輸入優惠碼" v-model="couponCode">
+        <div class="input-group-append">
+          <button class="btn btn-primary" type="button" :disabled="couponCode==''" @click="useCoupon">
+            套用優惠碼
+          </button>
+        </div>
+      </div>
+
+      <div class="my-5 row justify-content-center">
+        <validation-observer class="col-md-6" v-slot="{ invalid }">
+          <form @submit.prevent="createOrder">
+            <validation-provider rules="required|email" v-slot="{ errors, classes }">
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input id="email" type="email" name="email" v-model="form.user.email"
+                  placeholder="請輸入 Email"
+                  class="form-control" :class="classes">
+                <span class="invalid-feedback">{{ errors[0] }}</span>
+              </div>
+            </validation-provider>
+
+            <validation-provider rules="required|name" v-slot="{ errors, classes }">
+              <div class="form-group">
+                <label for="username">收件人姓名</label>
+                <input id="username" type="text" name="姓名" v-model="form.user.name"
+                  placeholder="請輸入收件人姓名"
+                  class="form-control" :class="classes">
+                <span class="invalid-feedback">{{ errors[0] }}</span>
+              </div>
+            </validation-provider>
+
+            <validation-provider rules="required|digits:10" v-slot="{ errors, classes }">
+              <div class="form-group">
+                <label for="usertel">收件人電話</label>
+                <input id="usertel" type="text" name="電話" v-model="form.user.usertel"
+                  placeholder="請輸入電話"
+                  class="form-control" :class="classes">
+                <span class="invalid-feedback">{{ errors[0] }}</span>
+              </div>
+            </validation-provider>
+
+            <validation-provider rules="required|address" v-slot="{ errors, classes }">
+              <div class="form-group">
+                <label for="useraddress">收件人地址</label>
+                <input id="useraddress" type="text" name="地址" v-model="form.user.useraddress"
+                  placeholder="請輸入地址"
+                  class="form-control" :class="classes">
+                <span class="invalid-feedback">{{ errors[0] }}</span>
+              </div>
+            </validation-provider>
+
+            <div class="form-group">
+              <label for="message">留言</label>
+              <textarea id="message" class="form-control" cols="30" rows="10"
+              v-model="form.message"></textarea>
             </div>
-          </td>
-          <td class="align-middle">{{ item.qty }} / {{ item.product.unit }}</td>
-          <td class="align-middle text-right">{{ item.final_total }}</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td colspan="3" class="text-right">總計</td>
-          <td class="text-right">{{ cart.total }}</td>
-        </tr>
-        <tr v-if="cart.total != cart.final_total">
-          <td colspan="3" class="text-right text-success">折扣價</td>
-          <td class="text-right text-success">{{ cart.final_total }}</td>
-        </tr>
-      </tfoot>
-    </table>
-    <div class="input-group mb-3 input-group-sm">
-      <input type="text" class="form-control" placeholder="請輸入優惠碼" v-model="couponCode">
-      <div class="input-group-append">
-        <button class="btn btn-primary" type="button" :disabled="couponCode==''" @click="useCoupon">
-          套用優惠碼
-        </button>
+
+            <div class="text-right">
+              <button class="btn btn-danger" :disabled="invalid">送出訂單</button>
+            </div>
+          </form>
+        </validation-observer>
       </div>
     </div>
-
   </div>
 </template>
 
@@ -144,6 +202,15 @@ export default {
       status: {
         loadingItem: '',
         loadingToCart: ''
+      },
+      form: {
+        user: {
+          name: '',
+          email: '',
+          tel: '',
+          address: ''
+        },
+        message: ''
       }
     }
   },
@@ -237,6 +304,30 @@ export default {
           this.getCart()
         } else {
           this.$bus.$emit('message:push', `套用優惠券失敗: ${response.data.message}`, 'danger')
+        }
+      })
+    },
+    createOrder () {
+      const vm = this
+      const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/order`
+      const order = vm.form
+
+      vm.isLoading = true
+      this.$http.post(api, { data: order }).then(response => {
+        vm.isLoading = false
+        if (response.data.success) {
+          this.getCart()
+          vm.form = {
+            user: {
+              name: '',
+              email: '',
+              tel: '',
+              address: ''
+            },
+            message: ''
+          }
+        } else {
+          this.$bus.$emit('message:push', `建立訂單失敗: ${response.data.message}`, 'danger')
         }
       })
     }
--- src/main.js
+++ src/main.js
@@ -9,6 +9,18 @@ import Datetime from 'vue-datetime'
 import 'vue-datetime/dist/vue-datetime.css'
 import 'bootstrap'
 
+// 導出 vee-validate 相關設定檔
+import {
+  ValidationObserver, // form 驗證元件
+  ValidationProvider, // input 驗證元件
+  extend, // 擴充功能
+  localize, // 語系設定
+  configure // 設定檔
+} from 'vee-validate'
+import TW from 'vee-validate/dist/locale/zh_TW.json'
+// 導入所有 vee-validate 驗證規則
+import * as rules from 'vee-validate/dist/rules'
+
 import App from './App'
 import router from './router'
 import './bus'
@@ -17,6 +29,42 @@ import datetimeFilter from './filters/datetime'
 
 Vue.use(VueAxios, axios)
 Vue.use(Datetime)
+
+// 手動導出 vee-validate 所有規則，再加入 vee-validate 擴充
+Object.keys(rules).forEach((rule) => {
+  extend(rule, rules[rule])
+})
+extend('name', {
+  validate: value => {
+    const pattern = /^[\u4e00-\u9fa5a-zA-Z]+$/
+    const re = new RegExp(pattern)
+    return re.test(value)
+  },
+  message: '姓名僅允許中文與英文'
+})
+extend('address', {
+  validate: value => {
+    const pattern = /^([\u4e00-\u9fa5]+\d*)+$/
+    const re = new RegExp(pattern)
+    return re.test(value)
+  },
+  message: '地址不符合規則'
+})
+
+// 設定語系
+localize('zh_TW', TW)
+// 針對單一元件與表單進行驗證
+Vue.component('ValidationObserver', ValidationObserver)
+Vue.component('ValidationProvider', ValidationProvider)
+
+// 針對 bootstrap 進行驗證的 classname 設定
+configure({
+  classes: {
+    valid: 'is-valid',
+    invalid: 'is-invalid'
+  }
+})
+
 Vue.config.productionTip = false
 
 Vue.component('Loading', Loading)
