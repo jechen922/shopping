74689f24e5cf8284b8fc2a9ef58c9f3f53ee3cce
Robert <example@gmail.com>
2021-04-15 13:42:59 +0800

    調整後台新增產品 modal 標題的背景顏色為 primary

--- src/components/pages/Products.vue
+++ src/components/pages/Products.vue
@@ -41,7 +41,7 @@
       aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content border-0">
-          <div class="modal-header bg-dark text-white">
+          <div class="modal-header bg-primary text-white">
             <h5 class="modal-title" id="exampleModalLabel">
               <span>新增產品</span>
             </h5>
